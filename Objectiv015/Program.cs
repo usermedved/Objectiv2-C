51c94d45762f85093029f08add77fa0261b83bc2
Sergei Medvedev <clip1988@yandex.ru>
2022-08-14 23:37:44 +0300

    Objectiv 015 project

---
+++ Objectiv015/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите цифру дня недели: "); 
+int numberDay = Convert.ToInt32(Console.ReadLine());
+if(numberDay == 6 || numberDay == 7) Console.Write("Это выходной день");
+else if(numberDay < 1 || numberDay > 7) Console.WriteLine("Это не день недели");
+else Console.WriteLine("Это буднечный день");
