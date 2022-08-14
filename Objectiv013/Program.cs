b985e1378e0b50c4989e49eb03a6af97ea4d358d
Sergei Medvedev <clip1988@yandex.ru>
2022-08-14 13:12:03 +0300

    Objectiv 013 project

---
+++ Objectiv013/Program.cs
@@ -0,0 +1,13 @@
+﻿int namber = new Random().Next(10,1000);
+Console.WriteLine($"Рандомное число: {namber}");
+int Digit = namber / 100;
+int thirdDigit;
+if (Digit == 0)
+{
+    Console.WriteLine("Третьего числа нет!!!");
+}
+else
+{
+    thirdDigit = namber % 10;
+    Console.WriteLine($"Третья цыфра рандомного числа: {thirdDigit}");
+}
